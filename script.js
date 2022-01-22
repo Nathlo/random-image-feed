0d8208879c2bed0a06799a19b02e97289485cf7c
NathLo <thyalan.aurlence@gmail.com>
2022-01-22 15:11:41 -0400

    styles added + reduced number of rows

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector('.container');
 const unsplashURL = 'https://source.unsplash.com/random/';
-const rows = 10;
+const rows = 5;
 
 for(let i = 0; i < rows * 3; i++) {
     const img = document.createElement('img');
